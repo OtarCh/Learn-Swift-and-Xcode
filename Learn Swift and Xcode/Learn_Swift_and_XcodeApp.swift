c21868511259f93b8a9945545b3301b011bf6dad
Otar Chadunashvili <oter.chadunashvili@gmail.com>
2022-04-10 19:20:34 +0400

    Initial Commit

---
+++ Learn Swift and Xcode/Learn_Swift_and_XcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_Swift_and_XcodeApp.swift
+//  Learn Swift and Xcode
+//
+//  Created by otar chadunashvili on 10.04.22.
+//
+
+import SwiftUI
+
+@main
+struct Learn_Swift_and_XcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
