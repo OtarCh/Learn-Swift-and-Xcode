0e7b309a06be32ead3283997c4515abcf1ae19f7
Otar Chadunashvili <oter.chadunashvili@gmail.com>
2022-04-10 20:28:53 +0400

    Add MyModel to the Project

---
+++ Learn Swift and Xcode/MyModel.swift
@@ -0,0 +1,15 @@
+//
+//  MyModel.swift
+//  Learn Swift and Xcode
+//
+//  Created by otar chadunashvili on 10.04.22.
+//
+
+import Foundation
+
+
+struct MyModel {
+    
+    var name:String?
+    
+}
