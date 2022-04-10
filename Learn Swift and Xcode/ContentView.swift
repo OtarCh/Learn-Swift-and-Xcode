aa67b0e580831099e0400fe47d20f51804dd9161
OtarCh <103374086+OtarCh@users.noreply.github.com>
2022-04-10 20:40:06 +0400

    Update ContentView to fun

--- Learn Swift and Xcode/ContentView.swift
+++ Learn Swift and Xcode/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Learning Github")
+        Text("Learning Github - It is easey and fun!")
             .padding()
     }
 }
